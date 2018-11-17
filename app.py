dd1a732e4e24017eb63648cc98f2731a4a8a2559
Rasmus Olofzon <rasmus.olofzon@gmail.com>
2018-11-17 13:56:05 +0100

    added skeleton for a very basic flask+bootstrap app, to build on

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+application = Flask(__name__)
+
+@application.route("/")
+def landing():
+	return render_template('index.html', page="home")
+
+@application.route("/yousave")
+def kontakt():
+	return render_template('yousave.html', page="yousave")
+
+if __name__ == "__main__":
+	application.run(host='0.0.0.0')
