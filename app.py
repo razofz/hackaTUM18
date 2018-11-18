c754845ba22c6809b3e10d14617564d10a26ab49
Can Bilgin <canbilgin.f@gmail.com>
2018-11-18 06:05:56 +0100

    Final version

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def personalised():
     if len(array) == 0:
         for station in stations_array:
             array.append(station.name)
-    
+
 
     if request.method == 'GET':
         return render_template('personalised.html', post=False, stations=array)
@@ -28,14 +28,14 @@ def personalised():
         toStation = getStation(request.form['to'])
         distance = getStationsDistance(fromStation, toStation)
 
-        if fromStation.name == "" || toStation.name == "":
+        if fromStation.name == "" or toStation.name == "":
             correct_stations = False
 
-        return render_template('personalised.html', 
-            car_monthly=(car.getMonthlyPriceGas(distance)+car.getMonthlyLossofValue(distance)), 
-            car_daily=(car.getDailyPriceGas(distance)+car.getDailyLossofValue(distance)), 
-            public_transport_monthly=getMonthlyPriceTransportation(fromStation, toStation), 
-            public_transport_daily=getDailyPrice(fromStation, toStation), 
+        return render_template('personalised.html',
+            car_monthly=(car.getMonthlyPriceGas(distance)+car.getMonthlyLossofValue(distance)),
+            car_daily=(car.getDailyPriceGas(distance)+car.getDailyLossofValue(distance)),
+            public_transport_monthly=getMonthlyPriceTransportation(fromStation, toStation),
+            public_transport_daily=getDailyPrice(fromStation, toStation),
             post=True, stations=array, alert_user=(not correct_stations))
 
 if __name__ == "__main__":
